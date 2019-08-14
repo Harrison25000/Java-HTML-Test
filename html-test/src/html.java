b0e8b6c7829e19565a8f0061343a6b954e7d41e2
Oliver <ofcharrison@gmail.com>
2019-08-14 12:15:26 +0100

    hello

--- html-test/src/html.java
+++ html-test/src/html.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 
-public class html{
+public class Html{
     public static void main(String[] args){
         File f = new File("./test.html");
         System.out.println("HI");
