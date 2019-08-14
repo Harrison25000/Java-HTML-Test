2c5bfadefc8f6968e84d211db6eac11d2e8e8a03
Oliver <ofcharrison@gmail.com>
2019-08-14 11:52:25 +0100

    setting up

---
+++ src/html.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class html{
+    public static void main(String[] args){
+        File f = new File("./test.html");
+        System.out.println("HI");
+        try{
+            BufferedWriter bw
+        }
+    }
+
+}
