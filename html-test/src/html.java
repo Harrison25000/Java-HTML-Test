10b6770f3378eaa253e50b14804531a2c280c493
Oliver <ofcharrison@gmail.com>
2019-08-14 12:05:08 +0100

    Finally setup properly - requires setting up module

--- html-test/src/html.java
+++ html-test/src/html.java
@@ -1,12 +1,10 @@
 import java.io.File;
+import java.io.FileWriter;
 
 public class html{
     public static void main(String[] args){
         File f = new File("./test.html");
         System.out.println("HI");
-        try{
-            BufferedWriter bw
-        }
     }
 
 }
